f6b2fa9cfc639c9001338d255719c3734c5c1262
Sreelal TS <heysreelal@gmail.com>
2022-06-06 00:41:01 +0530

    Memeasure some types 🏋🏻‍♀️

---
+++ c/set1/size-of-data-types.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of Character %d\n", sizeof(char));
+    printf("Size of Integer %d\n", sizeof(int));
+    printf("Size of Float %d\n", sizeof(float));
+    printf("Size of Double %d\n", sizeof(double));
+    printf("Size of Long %d\n", sizeof(long));
+    printf("Size of Long Long %d\n", sizeof(long long));
+    printf("Size of Short %d\n", sizeof(short));
+    printf("Size of Unsigned Character %d\n", sizeof(unsigned char));
+    printf("Size of Unsigned Integer %d\n", sizeof(unsigned int));
+    return 0;
+}
